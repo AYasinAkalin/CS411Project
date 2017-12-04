d7b9c2e0e86e53591edf54c4567307e0bb331f8f
AYasin <ayasinakalin@gmail.com>
2017-12-04 17:19:36 +0300

    Initial commit

---
+++ transactionGenerator.py
@@ -0,0 +1,40 @@
+# CS 411 507 2017 Fall Project - Phase 1
+# =====================================
+# Team Members
+# =====================================
+# Ali Yasin Akalin
+# Burak Aytac
+# Eylul Dicle Yurdakul
+
+
+# =====================================
+# Workload
+# =====================================
+'''
+In this phase of the project, you will generate random transactions following
+the format described.
+You are required to use SHA3 with 256-bit output
+as the cryptographic hash function.
+
+Provide a PoW for a single transcation.
+Generate at least ten transactions and write them to a file.
+'''
+
+# =====================================
+# Libraries
+# =====================================
+
+
+# =====================================
+# Functions
+# =====================================
+
+
+# =====================================
+# Initials
+# =====================================
+
+
+# =====================================
+# Main
+# =====================================
