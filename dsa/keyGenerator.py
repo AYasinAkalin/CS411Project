b3cfbee0691f4d19efaba9a302c6b2ef107befb2
AYasin <ayasinakalin@gmail.com>
2017-12-09 23:50:03 +0300

    Create files needed for phase 2

---
+++ dsa/keyGenerator.py
@@ -0,0 +1,47 @@
+# CS 411 507 2017 Fall Project - Phase 2
+# =====================================
+# Team Members
+# =====================================
+# Ali Yasin Akalin
+# Burak Aytac
+# Eylul Dicle Yurdakul
+
+
+# =====================================
+# Workload
+# =====================================
+'''
+Generate public (beta) and private (alpha) keys respectively.
+Use p, q, and g generated before. Read them from a .txt file.
+Create two .txt files.
+Put (q, p, g, alpha) and (q, p, g, beta) into files respectively.
+
+alpha is taken from user. It should satisfy:
+0 < alpha < q
+
+beta = g^alpha (mod p)
+'''
+
+# =====================================
+# Libraries
+# =====================================
+
+# =====================================
+# Functions
+# =====================================
+
+# =====================================
+# Initials
+# =====================================
+
+# =====================================
+# Main
+# =====================================
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ dsa/paramGenerator.py
@@ -0,0 +1,44 @@
+# CS 411 507 2017 Fall Project - Phase 2
+# =====================================
+# Team Members
+# =====================================
+# Ali Yasin Akalin
+# Burak Aytac
+# Eylul Dicle Yurdakul
+
+
+# =====================================
+# Workload
+# =====================================
+'''
+Generate prime number p, q; and a generator g. Write them in a .txt file.
+
+p is 256 bit integer.
+q is 2048 bit integer.
+p|q-1: p divides q minus 1
+g is a generator. It generates a subgroup of Z*_p with q number of elements.
+'''
+
+# =====================================
+# Libraries
+# =====================================
+
+# =====================================
+# Functions
+# =====================================
+
+# =====================================
+# Initials
+# =====================================
+
+# =====================================
+# Main
+# =====================================
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
