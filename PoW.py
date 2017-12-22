6849745b3f5ca58bfec7d07850b609653b88682e
AYasin <ayasinakalin@gmail.com>
2017-12-22 16:42:23 +0300

    Add module and function comments

--- PoW.py
+++ PoW.py
@@ -8,9 +8,21 @@
 
 
 # =====================================
-# Workload
+# Info
 # =====================================
 '''
+Generates Proof of Work (PoW) values for previously signed Bitcoin transaction
+blocks, writes generated parameters (Previous Pow, Merkle Hash, Nonce, PoW) to
+both terminal and a file.
+
+Requires custom python module(s):
+    */library/transactionHelpers.py
+
+Functions
+    generateMerkleHash(fileName, lenTx): Returns a hash value from a Tx Block
+    findPrevPoW(fName): Returns PoW hash value of previous Tx Block
+    PoW(TxBlockFile, ChainFile, PoWLen, TxLen): Generates PoW for a Tx Block
+        with TxLen number of '0's in the beginning of it
 '''
 
 
@@ -25,13 +37,23 @@ import sha3
 # =====================================
 # Functions
 # =====================================
-# lenTx is the length of a single transaction information written in the file
 def generateMerkleHash(fileName, lenTx):
-    ''' 
-    Function explanation
-    lenTx: (int) Number of lines holding info for a single transaction
     '''
-    content = TxLib.readInputFile(fileName)
+    Takes two parameters, returns a single hexadecimal hash value as 'string'
+    Opens file containing 2^n number of bitcoin transaction informations.
+    Each transaction hashes into a single value. Then each pair of hash value
+    hashes into a new single value. This operation keeps going until number of
+    hash value is decreased to 1.
+
+    Input(s)
+    fileName: (string) Name of the file containing a block of transaction info
+    lenTx: (int) Number of lines holding info for a single transaction
+
+    Output
+    A 'string' value. It is a hash (SHA3_256) value
+        of a transaction block in hex format.
+    '''
+    content = TxLib.readInputFile(fileName)  # Content of transaction info file
     hashList = []
     numOfTxs = len(content) / lenTx
 
@@ -53,7 +75,18 @@ def generateMerkleHash(fileName, lenTx):
     return hashList[0]
 
 
-def findPrevPoW(fName):
+def findPrevPoW(fName=''):
+    '''
+    Takes a parameter, a file name as string. Returns a string, previous PoW
+    hash value of previous transaction block, by either reading the value from
+    a file of which name is given or finding it from a global variable, a list.
+
+    Input
+    fName: (string) Name of the file containing PoW hash values
+
+    Output
+    A 'string' value. It is a PoW of a transaction block in hexadecimal format.
+    '''
     if len(listPoWs) is 0:
         try:
             lines = TxLib.readInputFile(fName)
@@ -69,6 +102,33 @@ def findPrevPoW(fName):
 
 
 def PoW(TxBlockFile, ChainFile, PoWLen, TxLen):
+    '''
+    Takes 4 parameters. Returns nothing. Calculates a proof of work value per
+    transaction block which is related to block comes before it.
+    Prints 4 values and some informative messages to screen
+    and writes those values to a file.
+
+    Inputs
+    TxBlockFile: (string) Name of the file containing a block of transaction info
+    ChainFile: (string) Name of the file outputs of this function will be written on
+    PoWLen: (int) Number of consequtive zeros required at the beginning of PoW hash
+    TxLen: (int) Number of lines in a single transaction
+
+    Output on the screen:
+    Looking for a proof of work value.
+    Found one.
+    Previous PoW Day Zero Link in the Chain
+    hashMerkle:  0a5ef716fa67793eb4e9c749f12b7d2ef6e029260d0412fb2fbb7b20d9140c63
+    nonce:  336787476443477213007589513908532830366
+    Current PoW:  0007eabc927c21078c05f62db9be9c82f25951e92a2ff20f5c55c160a29ea95c
+    ========================================
+
+    Output on the file:
+    Day Zero Link in the Chain
+    992f1da6338e1813f41e3fe46d69b2617cf73af640871c06167646a86b3bb765
+    314659532392970585142622764647369548167
+    0000e5961e1a639707aa7ebce5e2227a23acc33f44e1a1b3ac6a74d224af413f
+    '''
     powPrev = findPrevPoW(ChainFile)
     hashMerkle = generateMerkleHash(TxBlockFile, TxLen)
     powCurrent = ''
@@ -97,8 +157,7 @@ IOFolderDir = '/Outputs'              # Directory of I/O files
 nameTxBlockFile = 'TransactionBlock'  # ie. TransactionBlock2.txt
 nameChainFile = 'LongestChain.txt'    # Name of output file
 PoWLen = 6  # Number of consequtive zeros at the beginning of PoW hash value
-# Number of lines holding info for single transaction
-TxLen = 10  # Number of lines in a transaction
+TxLen = 10  # Number of lines in a single transaction in a block file
 listPoWs = []
 
 
